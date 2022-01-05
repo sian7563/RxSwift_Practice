4d4833c202fd269db12061b4219e64a403462f11
sian7563 <sian7563@naver.com>
2022-01-05 22:51:45 +0900

    ⚡️ :: add view model files

---
+++ RxMemo/ViewModel/MemoComposeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MemoComposeViewModel.swift
+//  RxMemo
+//
+//  Created by kimsian on 2022/01/05.
+//
+
+import Foundation
+
+class MemoComposeViewModel {
+    
+}
---
+++ RxMemo/ViewModel/MemoListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MemoListViewModel.swift
+//  RxMemo
+//
+//  Created by kimsian on 2022/01/05.
+//
+
+import Foundation
+
+class MemoListViewModel {
+    
+}
