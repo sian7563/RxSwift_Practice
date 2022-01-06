7cf6059e300cdff4925536b5f550400cc02f716c
sian7563 <sian7563@naver.com>
2022-01-06 11:03:08 +0900

    ⚡️ :: add scene coordinator

---
+++ RxMemo/Scene/SceneCoordinatorType.swift
@@ -0,0 +1,18 @@
+//
+//  SceneCoordinatorType.swift
+//  RxMemo
+//
+//  Created by kimsian on 2022/01/05.
+//
+
+import Foundation
+import RxSwift
+
+protocol SceneCoordinatorType {
+    @discardableResult
+    func transition(to scene: Scene, using style: TransitionStyle, animated: Bool)
+       -> Completable
+    
+    @discardableResult
+    func close(animated: Bool) -> Completable
+}
